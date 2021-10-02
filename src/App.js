f5d39f3f0846e8a8e4e6a429cc794e5aabb93304
Tapish Kumbhare <kumbharetapish12@gmail.com>
2021-10-03 00:13:28 +0530

    package updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from "react";
-import Container from "./Containers/Container";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import Footer from "../src/Components/Footer/Footer";
+import Topbar from "../src/Components/Topbar/Topbar";
+import Account from "../src/Containers/Accounts/Account";
+import AddNewProduct from "../src/Containers/AddNewProduct/AddNewProduct";
+import Dashboard from "../src/Containers/Dashboard/Dashboard";
+import Login from "../src/Containers/Login/Login";
+import Product from "../src/Containers/Products/Product";
+import {
+  Account_Link,
+  Add_New_Link,
+  Dashboard_Link,
+  Login_Link,
+  Product_Link,
+} from "../src/Utils/Network";
 
-function App() {
-  return <Container />;
+function App(props) {
+  return (
+    <Router>
+      <div className="container">
+        <Topbar />
+
+        <Switch>
+          <Route
+            exact
+            path={Dashboard_Link}
+            render={(data) =>
+              props.loginStatus ? <Dashboard {...data} /> : <Redirect to={Login_Link} />
+            }
+          />
+          <Route path={Product_Link} component={Product} />
+          <Route path={Account_Link} component={Account} />
+          <Route path={Login_Link} render={(routeProps) => <Login {...routeProps} />} />
+          <Route path={Add_New_Link} component={AddNewProduct} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { count: state.counter, loginStatus: state.status };
+};
+
+export default connect(mapStateToProps)(App);
